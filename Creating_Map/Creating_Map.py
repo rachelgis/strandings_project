b93c37c8af8439cffc40953634edfc23cf70e0e8
rachelgis <101409455+rachelgis@users.noreply.github.com>
2022-04-26 21:32:14 +0100

    cointinuing work plotting data to map

--- Creating_Map/Creating_Map.py
+++ Creating_Map/Creating_Map.py
@@ -1,3 +1,4 @@
+#Creating map branch of my marine mammals stranding project
 #importing the files i will use
 
 import cartopy.crs as ccrs
@@ -6,8 +7,9 @@ import matplotlib.pyplot as plt
 from cartopy.feature import ShapelyFeature
 import rasterio as rst
 import pandas as pd
+from geopandas import GeoDataFrame
 from shapely.geometry import Point
-plt.ion() # making the plotting interactive
+from shapely.geometry import MultiPoint
 
 #load the data
 outline = gpd.read_file('Data_Files/Scotland_boundary/Scotland boundary.shp') #outline of Scotland
@@ -17,22 +19,21 @@ counties = gpd.read_file('Data_Files/Localities2020boundaries/Localities2020boun
 strandings = pd.read_csv('Data_Files/Strandings_SMASS _data1992_2021_utf8.txt') #importing strandings for conversion
 
 #convert mammal strandings database into points shapefile
-strandings['geometry'] = list(zip(strandings['LatitudeWGS84'], strandings['LongitudeWGS84']))
+strandings['geometry'] = list(zip(strandings['LongOSGB36'], strandings['LatOSGB36']))
 strandings['geometry'] = strandings['geometry'].apply(Point)
-del strandings['LatitudeWGS84'], strandings['LongitudeWGS84']
-strandingsgdf = gpd.GeoDataFrame(strandings)
+strandingsgdf: GeoDataFrame = gpd.GeoDataFrame(strandings)
 strandingsgdf.set_geometry('geometry')
-strandingsgdf.set_crs("EPSG:4326", inplace=True) #sets the coordinate reference system to epsg:4326, wgs84 lat/lon
+strandingsgdf.set_crs("EPSG:4277", inplace=True) #sets the coordinate reference system to epsg:3857 WGS84 lat/lon
 strandingsgdf.to_file('marinestrandings.shp')
-print(strandingsgdf)
+
 
 #load the new strandings shapefile
 marinestrandings = gpd.read_file('marinestrandings.shp')
+marinestrandings.to_crs("OSGB 1936 / British National Grid")
 
 #creating the map
-myFig = plt.figure(figsize=(10, 10))  # create a figure of size 10x10 (representing the page size in inches)
-myCRS = ccrs.UTM(30)  # create a Universal Transverse Mercator reference system to transform our data.
-# be sure to fill in XX above with the correct number for the area we're working in.
+myFig = plt.figure(figsize=(10, 10))  # create a figure of size 15x15 (representing the page size in inches)
+myCRS = ccrs.UTM(30)  # create a reference system to transform our data.
 ax = plt.axes(projection=ccrs.Mercator())  # finally, create an axes object in the figure, using a Mercator
 # projection, where we can actually plot our data.
 
@@ -46,8 +47,12 @@ counties_feat = ShapelyFeature(counties['geometry'], myCRS, edgecolor='mediumblu
 ax.add_feature(counties_feat)
 
 #adding strandings
-strandings_feat = ShapelyFeature(marinestrandings['geometry'], myCRS, edgecolor='coral', facecolor='coral', linewidth=2)
-ax.add_feature(strandings_feat)
+#strandings_feat = ShapelyFeature(marinestrandings['geometry'], myCRS, edgecolor='coral', facecolor='coral', linewidth=3)
+#ax.add_feature(strandings_feat)
+plt.scatter()
+
+
+
 # using the boundary of the shapefile features, zoom the map to our area of interest
 ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS) # because total_bounds gives output as xmin, ymin, xmax, ymax,
 # but set_extent takes xmin, xmax, ymin, ymax, we re-order the coordinates here.
